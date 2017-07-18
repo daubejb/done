c0e2a86d8143a44e0b490145922071cc14c60bab
Jeffrey B. Daube <daubejb@gmail.com>
2017-07-17 21:07:20 -0400

    got item to post to google sheet

--- interface/api.py
+++ interface/api.py
@@ -11,45 +11,63 @@ from oauth2client.file import Storage
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 CLIENT_SECRET_FILE = 'client_secrets.json'
 APPLICATION_NAME = 'done'
+RANGE = '2done!A2:E1000'
+SPREADSHEET_ID = '1WIlw6BvlQtjXO9KtnT4b6XY8d3qAaK5RYDRnzekkVjM'
+
 
 class GoogleAPI:
     '''Represents the v4 google api for interacting with google apps'''
+
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   CLIENT_SECRET_FILE)
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+
+    http = credentials.authorize(httplib2.Http())
+    discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
+                    'version=v4')
+    service = discovery.build('sheets', 'v4', credentials=credentials)
+    print(service)
     def __init__(self):
-        '''Initializes an instance of the google API'''
+        '''Instantiates a google api'''
 
-    def get_credentials(self):
-        '''Gets valid user credentials from storage.
-        If nothing has been stored, or if the stored credentials are invalid,
-        the OAuth2 flow is completed to obtain the new credentials.
-        Returns:
-                Credentials, the obtained credential.'''
+    def process_api_call(self, values, api_type):
+        '''Receives an api request and processes it based on the arguments.'''
+        if api_type == 'append_item':
+            self.append_item_to_sheet(values)
 
-        home_dir = os.path.expanduser('~')
-        credential_dir = os.path.join(home_dir, '.credentials')
-        if not os.path.exists(credential_dir):
-            os.makedirs(credential_dir)
-        credential_path = os.path.join(credential_dir,
-                                       CLIENT_SECRET_FILE)
+    def append_item_to_sheet(self, values):
+        body = {
+            "range": RANGE,
+            "majorDimension": 'ROWS',
+            "values": [
+                values
+            ],
+        }
+        s = self.service
+        s.spreadsheets().values().append(
+            spreadsheetId=SPREADSHEET_ID, range=RANGE,
+            valueInputOption='USER_ENTERED',
+            body=body).execute()
 
-        store = Storage(credential_path)
-        credentials = store.get()
-        if not credentials or credentials.invalid:
-            flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-            flow.user_agent = APPLICATION_NAME
-            if flags:
-                flags = tools.argparser.parse_args(args=[])
-                credentials = tools.run_flow(flow, store, flags)
-            else:  # Needed only for compatibility with Python 2.6
-                credentials = tools.run(flow, store)
-            print('Storing credentials to ' + credential_path)
-        return credentials
-
-
-    def instantiate_api_service(self, credentials):
-        http = credentials.authorize(httplib2.Http())
-        # API Call
-        discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
-                        'version=v4')
-        service = discovery.build('sheets', 'v4', http=http,
-                                  discoveryServiceUrl=discoveryUrl)
-        return service
--- main.py
+++ main.py
@@ -3,17 +3,15 @@ from terminal.terminal import Terminal
 from interface.api import GoogleAPI
 
 def main():
-    google_api = GoogleAPI()
-    credentials = google_api.get_credentials()
-    service = google_api.instantiate_api_service(credentials)
     terminal = Terminal()
     args = terminal.get_user_input()
     todo_list = TodoList('done')
+    google_api = GoogleAPI()
+#    credentials = google_api.get_credentials()
+#    service = google_api.instantiate_api_service(credentials)
     values = terminal.evaluate_user_input(args)
-    print(values)
-    all_list_data = todo_list.get_list_data(service)
-    print(all_list_data)
-
+    google_api.process_api_call(values, 'append_item')
+    
 
 if __name__ == '__main__':
     main()
