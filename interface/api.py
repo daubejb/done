4b5e07bd23b940aed45626a49f14d32b36da6a41
Jeffrey B. Daube <daubejb@gmail.com>
2017-07-24 16:26:03 -0400

    fix google api access due to argparse issue

--- interface/api.py
+++ interface/api.py
@@ -3,14 +3,14 @@ import os.path
 import time
 import httplib2
 
-from apiclient import discovery
+from googleapiclient import discovery
 from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
 
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-CLIENT_SECRET_FILE = 'client_secrets.json'
+CLIENT_SECRET_FILE = '/home/jedaube/done/interface/done_client_secret.json'
 APPLICATION_NAME = '2done'
 RANGE = '2done!A2:E1000'
 DONE_RANGE = 'done!A2:F1000'
@@ -32,14 +32,14 @@ class GoogleAPI:
     credential_dir = os.path.join(home_dir, '.credentials')
     if not os.path.exists(credential_dir):
         os.makedirs(credential_dir)
-    credential_path = os.path.join(credential_dir,
-                                   CLIENT_SECRET_FILE)
+    credential_path = os.path.join(credential_dir, 'done_client_secret.json')
 
     store = Storage(credential_path)
     credentials = store.get()
     if not credentials or credentials.invalid:
         flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
         flow.user_agent = APPLICATION_NAME
+        flags = tools.argparser.parse_args(args=[])
         if flags:
             credentials = tools.run_flow(flow, store, flags)
         else:  # Needed only for compatibility with Python 2.6
